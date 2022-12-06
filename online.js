f388bc555f0cb171b9aef3a81a41c5222b263d36
Yashraj2607 <yr58907@gmail.com>
2022-12-07 01:48:30 +0530

    login

--- online.js
+++ online.js
@@ -1,2 +1,11 @@
 var email = "yraj9248@gmail.com";
 var password = "123@"
+
+var login_email="yraj9248@gmail.com";
+var login_password = "123@";
+
+if(email===login_email && password===login_password){
+console.log("succesfull
+}else{
+console.log("wrong credation")
+}
